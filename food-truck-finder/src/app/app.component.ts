87130162dd8e586d2275ed5cfb5c39599ceb8ef6
Houston <houstonkoester@aol.com>
2021-07-19 13:20:05 -0700

    Added map/markers/hard coded menu switch

--- food-truck-finder/src/app/app.component.ts
+++ food-truck-finder/src/app/app.component.ts
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Food Truck Finger';
+  title = 'Food Truck Finder';
   subTitle = 'finding food trucks for folks';
 }
