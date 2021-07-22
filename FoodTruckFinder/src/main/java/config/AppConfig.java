67544c97de45df18ac22dab732a88d93c10d94ac
HoustonKoester <67532088+HoustonKoester@users.noreply.github.com>
2021-07-22 11:26:43 -0700

    Delete FoodTruckFinder directory

--- FoodTruckFinder/src/main/java/config/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package config;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"aspectContainer", "database", "services"})
-public class AppConfig {
-}
